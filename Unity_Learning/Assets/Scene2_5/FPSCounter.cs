1e6bb6caa42226a2cf5f1f2f7d72841718b92cfc
TH <tangjinqi@qiyi.com>
2020-01-21 17:33:07 +0800

    【update】 更新Scene2_5

---
+++ Unity_Learning/Assets/Scene2_5/FPSCounter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int FPS { get; private set; }
+    public int AverageFPS { get; private set; }
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+    public int frameRange = 60;
+
+    int[] fpsBufffer;
+    int fpsBufferIndex;
+
+    void InitializeBuffer ()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBufffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void UpdateBuffer()
+    {
+        fpsBufffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange) {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS ()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++)
+        {
+            int fps = fpsBufffer[i];
+            sum += fps;
+            if (fps > highest)
+            {
+                highest = fps;
+            }
+            if (fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fpsBufffer == null || fpsBufffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+        //FPS = (int)(1f / Time.unscaledDeltaTime);
+    }
+}
---
+++ Unity_Learning/Assets/Scene2_5/NuclearSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuclearSpawner : MonoBehaviour
+{
+    
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
